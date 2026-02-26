900ec57bcb1d0c6a158f42004d50883b61afc87b
pixelFeft <igor1811yaz@gmail.com>
2026-02-26 17:34:24 +0500

    Update SeedOre.java

--- src/main/java/me/pixelfeft/SeedOre.java
+++ src/main/java/me/pixelfeft/SeedOre.java
@@ -1,24 +1,34 @@
 package me.pixelfeft;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.minecraft.server.command.CommandManager.*;
+import com.mojang.brigadier.arguments.LongArgumentType;
+
 public class SeedOre implements ModInitializer {
 	public static final String MOD_ID = "seedore";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	// Здесь мы будем хранить сид сервера
+	public static long serverSeed = 0;
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("SeedOre: Мод загружен. Готов к поиску руды по сиду!");
 
-		LOGGER.info("Hello Fabric world!");
+		// Регистрируем команду /setseed <число>
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(literal("setseed")
+				.then(argument("seed", LongArgumentType.longArg())
+					.executes(context -> {
+						serverSeed = LongArgumentType.getLong(context, "seed");
+						context.getSource().sendFeedback(() -> Text.literal("Сид установлен: " + serverSeed), false);
+						return 1;
+					})));
+		});
 	}
 }
