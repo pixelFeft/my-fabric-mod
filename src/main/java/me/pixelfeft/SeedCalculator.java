85292ad5df239732bd4b3eceab435ca50d6b775d
pixelFeft <igor1811yaz@gmail.com>
2026-02-26 19:16:43 +0500

    Update SeedCalculator.java

--- src/main/java/me/pixelfeft/SeedCalculator.java
+++ src/main/java/me/pixelfeft/SeedCalculator.java
@@ -2,9 +2,9 @@ package me.pixelfeft;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.gen.random.ChunkRandom;
-import net.minecraft.world.gen.random.RandomSeed;
-import net.minecraft.world.gen.random.Xoroshiro128PlusPlusRandomImpl;
+import net.minecraft.util.math.random.ChunkRandom;
+import net.minecraft.util.math.random.RandomSeed;
+import net.minecraft.util.math.random.CheckedRandom;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +14,16 @@ public class SeedCalculator {
     public static List<BlockPos> getOreInChunk(long seed, ChunkPos pos) {
         List<BlockPos> locations = new ArrayList<>();
         
-        // В 1.21.10 используется алгоритм Xoroshiro для рандома
-        ChunkRandom random = new ChunkRandom(new Xoroshiro128PlusPlusRandomImpl(RandomSeed.getSeed()));
+        // В 1.21.10 рандом создается через CheckedRandom или LocalRandom
+        ChunkRandom random = new ChunkRandom(new CheckedRandom(RandomSeed.getSeed()));
         
-        // Устанавливаем сид для конкретного чанка (логика алмазов)
+        // Устанавливаем сид для конкретного чанка
         random.setCarverSeed(seed, pos.x, pos.z);
         
-        // Пример упрощенной логики: в каждом чанке обычно 1-8 попыток генерации алмазов
+        // Алгоритм генерации алмазов (упрощенный пример)
         for (int i = 0; i < 8; i++) {
             int x = pos.getStartX() + random.nextInt(16);
-            int y = random.nextInt(64) - 64; // Глубина от -64 до 0
+            int y = random.nextInt(64) - 64; 
             int z = pos.getStartZ() + random.nextInt(16);
             
             locations.add(new BlockPos(x, y, z));
