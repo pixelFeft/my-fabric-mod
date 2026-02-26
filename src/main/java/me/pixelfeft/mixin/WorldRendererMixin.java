53abc1e1acc00ba4f98b82c2ba512c6f09db645a
pixelFeft <igor1811yaz@gmail.com>
2026-02-26 19:14:40 +0500

    Update WorldRendererMixin.java

--- src/main/java/me/pixelfeft/mixin/WorldRendererMixin.java
+++ src/main/java/me/pixelfeft/mixin/WorldRendererMixin.java
@@ -12,9 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
-    @Inject(method = "render", at = @At("RETURN"))
+@Inject(method = "render", at = @At("RETURN"))
     private void onRender(RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci) {
-        // Здесь мы будем вызывать отрисовку наших "фантомных" блоков
-        // Когда мы вычислим координаты руды, мы пустим их сюда через цикл
+        if (SeedOre.serverSeed == 0) return; // Если сид не введен, ничего не рисуем
+
+        // Здесь мы будем вызывать отрисовку линий (ESP)
+        // Для теста пока просто выведем в консоль, что мы нашли точку
+        // LOGGER.info("Рисую руду на координатах...");
     }
 }
